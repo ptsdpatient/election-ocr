1c662e61e1bb6f7a3a1bd6beac2cf3bd9167dfb5
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2025-11-29 12:14:56 +0530

    Update nanonet.py

--- nanonet.py
+++ nanonet.py
@@ -1,7 +1,7 @@
 import requests
 
-API_KEY = "c03e427e-96d6-11f0-bbac-364ad0a655a4"   # your key
-MODEL_ID = "eeea72a6-ca04-4c5a-bc22-c032a84e9b2f" # your model id
+API_KEY = ""   # your key
+MODEL_ID = "" # your model id
 
 url = f"https://app.nanonets.com/api/v2/OCR/Model/{MODEL_ID}/LabelFile/"
 files = {'file': open('./pdf/2.pdf', 'rb')}
